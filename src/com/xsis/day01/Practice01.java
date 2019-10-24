3dacc77309ad8bd8fb91151b8a1078971bec92ce
mujahidur rahman <mujahidur>
2019-10-24 11:14:18 +0700

    tambahan Practice01

--- src/com/xsis/day01/Practice01.java
+++ src/com/xsis/day01/Practice01.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 
 public class Practice01 {
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println("Hello World");
+        System.out.println(args [0]);
+        System.out.println(args [1]);
+        System.out.println(args [2]);
+        System.out.println("Hello Xsis Academy");
+        System.out.println("Hello \nAcademy");
+        System.out.println("Hello \tAcademy");
     }
 
 }
